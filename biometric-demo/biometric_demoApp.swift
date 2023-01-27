f00d9da6a200812ddfcb518acc40a67e02ba9445
youlserf <youlserf.cardenas@tecsup.edu.pe>
2023-01-26 19:30:59 -0800

    Initial Commit

---
+++ biometric-demo/biometric_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  biometric_demoApp.swift
+//  biometric-demo
+//
+//  Created by youlserf on 26/01/23.
+//
+
+import SwiftUI
+
+@main
+struct biometric_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
